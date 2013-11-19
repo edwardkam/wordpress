9a06e781142e2de0d52ca2bd1229755c39594e1f
root <root@ip-10-254-74-223.us-west-2.compute.internal>
2013-11-19 17:12:54 +0800

    first commit

---
+++ config/db.php
@@ -0,0 +1,22 @@
+<?php
+//
+// Cookbook Name:: app_php
+//
+// Copyright RightScale, Inc. All rights reserved.
+// All access and use subject to the RightScale Terms of Service available at
+// http://www.rightscale.com/terms.php and, if applicable, other agreements
+// such as a RightScale Master Subscription Agreement.
+//
+// Managed by RightScale
+// DO NOT EDIT BY HAND
+//
+// PHP database connection Config file
+//
+
+
+//DataBase Connect Infomation
+$hostname_DB = "localhost";
+$username_DB = "edward";
+$password_DB = "abc_123";
+$database_DB = "wordpress";
+?>
